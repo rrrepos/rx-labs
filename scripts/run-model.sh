c3cf0c7de4c44c8ebaa7c8b91771ea444481662e
rrrepos <connect2rmr@gmail.com>
2024-07-03 12:44:11 +0530

    created docker env script

--- scripts/run-model.sh
+++ scripts/run-model.sh
@@ -1,7 +1,16 @@
+if [[ -z "$1" ]]; then
+    echo "Token is empty"
+    exit 1
+fi
+
+hfprefix="HUGGING_FACE_HUB_TOKEN="
+HFTOKEN="$hfprefix""$1"
+
 MODEL="meta-llama/Meta-Llama-3-8B-Instruct" 
 
 docker run --runtime nvidia --gpus all \
     -v ~/.cache/huggingface:/root/.cache/huggingface \
+    --env-file docker.env \
     -p 8000:8000 \
     --ipc=host \
     vllm/vllm-openai:latest \
---
+++ scripts/set-hf-token.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+    echo "Token is empty"
+    exit 1
+fi
+
+export HUGGING_FACE_HUB_TOKEN="$1"
+
+env_file="scripts/docker.env"
+
+echo "HUGGING_FACE_HUB_TOKEN=$1" > $env_file
+
+
+echo "The Hugging Face token you have set is: ""$1"
+echo "--------------------------------------"
+echo "if wrong run the script again"
+
