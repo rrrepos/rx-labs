df8d4231aed41eb1e5804b040d56b57d900c692a
rrrepos <connect2rmr@gmail.com>
2024-07-03 13:11:36 +0530

    changed run-model script

--- scripts/run-model.sh
+++ scripts/run-model.sh
@@ -1,11 +1,3 @@
-if [[ -z "$1" ]]; then
-    echo "Token is empty"
-    exit 1
-fi
-
-hfprefix="HUGGING_FACE_HUB_TOKEN="
-HFTOKEN="$hfprefix""$1"
-
 MODEL="meta-llama/Meta-Llama-3-8B-Instruct" 
 
 docker run --runtime nvidia --gpus all \
