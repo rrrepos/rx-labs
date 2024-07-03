43a46570172667fc96c84efce095ced4ef602bd1
rrrepos <connect2rmr@gmail.com>
2024-07-03 13:27:07 +0530

    changed run-model script to include env-file

--- scripts/run-model.sh
+++ scripts/run-model.sh
@@ -2,7 +2,7 @@ MODEL="meta-llama/Meta-Llama-3-8B-Instruct"
 
 docker run --runtime nvidia --gpus all \
     -v ~/.cache/huggingface:/root/.cache/huggingface \
-    --env-file docker.env \
+    --env-file "docker.env" \
     -p 8000:8000 \
     --ipc=host \
     vllm/vllm-openai:latest \
